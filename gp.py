0f897c66333d1ffdb762c24097735a4c6336c59b
Justin Findlay <jfindlay@gmail.com>
2013-04-22 15:25:00 -0600

    gp.py: a simple, transparent python gnuplot interface

---
+++ gp.py
@@ -0,0 +1,109 @@
+# gp.py: a simple, transparent python gnuplot interface
+import os,sys
+from subprocess import Popen,PIPE
+
+ISO_8601 = '%Y-%m-%dT%H:%M:%S'
+
+class Gnuplot:
+  def __init__(self):
+    self.term = 'wxt'
+    self.canvas_size = (1024,600)
+    self.time_format = ISO_8601
+    self.axis_time_format = '%m-%d\\n%H:%M'
+    self.files = {}  # dictionary of files used to store data for plots
+    self.f_sep = '|' # data file field separator
+    self.history = open('%s/pygp.hist' % self._ensure_dir('%s/.cache' % os.getenv('HOME')),'w')
+    self.gp = Popen(('gnuplot',),stdout=PIPE,stdin=PIPE,close_fds=True)
+    self.stdin,self.stdout = self.gp.stdin,self.gp.stdout
+    self.write('set samples 128\n')
+    self.write('set isosamples 128\n')
+    self.write('set key top left\n')
+    self.set_datafile_separator()
+    self.set_term()
+
+  def __del__(self):
+    self.gp.stdin.close()
+    self.gp.stdout.close()
+    self.history.close()
+    for f_name in self.files.keys():
+      if not self.files[f_name].closed:
+        self.files[f_name].close()
+      if os.path.exists(f_name):
+        os.remove(f_name)
+
+  def _ensure_dir(self,directory):
+    '''
+    check whether the supplied directory exists, create it if it doesn't, and
+    exit if it is not a directory
+    '''
+    if not os.path.exists(directory):
+      if not os.path.islink(directory):
+        os.makedirs(directory)
+        return directory
+    elif os.path.isdir(directory):
+      return directory
+    sys.stderr.write('%s exists and is not a directory\n' % directory)
+    sys.stderr.flush()
+    sys.exit(1)
+
+  def write(self,a):
+    '''
+    submit command 'a' to gnuplot and write it to history
+    '''
+    self.stdin.write(a)
+    self.history.write(a)
+
+  def set_datafile_separator(self,f_sep=None):
+    if f_sep : s = f_sep
+    else : s = self.f_sep
+    self.set('datafile separator "%s"' % s)
+
+  def set_term(self,term=None,width=None,height=None):
+    '''
+    setup terminal
+    '''
+    if term : t = term
+    else : t = self.term
+    if width : x = width
+    else : x = self.canvas_size[0]
+    if height : y = height
+    else : y = self.canvas_size[1]
+    self.set('term %s size %d,%d' % (t,x,y))
+
+  def set_time_format(self,fmt=None):
+    if fmt : f = fmt
+    else : f = self.time_format
+    self.set('timefmt "%s"' % f)
+
+  def set(self,a=None):
+    '''
+    set gnuplot setting 'a'
+    '''
+    if a:
+      self.write('set %s\n' % a)
+
+  def write_file(self,f_name,*args):
+    '''
+    Write data to temp file used to construct gnuplot plots.  All args supplied
+    must be 1D iterables of the same size.
+    '''
+    self.files[f_name] = open(f_name,'w')
+    for row in xrange(len(args[0])):
+      line = ''
+      for arg in args:
+        if not isinstance(arg[row],str):
+          m = '%g' % arg[row]
+        else:
+          m = arg[row]
+        if len(line):
+          line += self.f_sep
+        line += m
+      self.files[f_name].write('%s\n' % line)
+    self.files[f_name].close()
+
+  def plot(self,a=None):
+    '''
+    plot with argument 'a'
+    '''
+    self.write('plot %s\n' % a)
+    self.gp.wait()
